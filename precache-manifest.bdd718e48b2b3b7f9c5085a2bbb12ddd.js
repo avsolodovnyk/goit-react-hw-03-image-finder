4e2340e967322c69b9fe1dedc808ee7829458a1c
Anton S <av.solodovnyk@gmail.com>
2019-11-06 01:21:43 +0200

    Updates

---
+++ precache-manifest.bdd718e48b2b3b7f9c5085a2bbb12ddd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f15d9f3948538bcea217ac4c89aea8b",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e5adeae7a37ca417c6a6",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.6537cfd4.chunk.css"
+  },
+  {
+    "revision": "12eb98c7017faedea5bf",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.2d059cb0.chunk.css"
+  },
+  {
+    "revision": "e5adeae7a37ca417c6a6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3471ae33.chunk.js"
+  },
+  {
+    "revision": "12eb98c7017faedea5bf",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.5802403a.chunk.js"
+  },
+  {
+    "revision": "26f980cacd962de52565",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9d9945f4.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/goit-react-hw-03-image-finder/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/goit-react-hw-03-image-finder/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/goit-react-hw-03-image-finder/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/goit-react-hw-03-image-finder/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
